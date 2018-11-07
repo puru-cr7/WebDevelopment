9cb42e0484e70e2b59c7670164bd6b17ef219fe4
puru_cr7 <puru.2008@gmail.com>
2018-11-07 18:39:14 +0530

    Sharing my 1st Django project. It has an app which can send emails via smtp

---
+++ Getting_started_with_Django/test_email_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestEmailAppConfig(AppConfig):
+    name = 'test_email_app'
---
+++ Getting_started_with_Django/test_email_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
+import datetime
+from django.core.mail import EmailMessage
+
+# Create your views here.
+def hello(request):
+   today = datetime.datetime.now().date()
+   daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+   return render(request, "hello.html", {"today" : today, "days_of_week" : daysOfWeek})
+
+def sendSimpleEmail(request):
+   res = send_mail("hello cute", "test mail, no need to reply", "from@gmail.com", ['to@gmail.com'])
+   return HttpResponse('%s'%res)
+
+def sendEmailWithAttach(request):
+   html_content = "Comment tu vas?"
+   email = EmailMessage("my subject", html_content, "from@gmail.com", ['to@gmail.com'])
+   email.content_subtype = "html"
+   
+   fd = open('manage.py', 'r')
+   email.attach('manage.py', fd.read(), 'text/plain')
+   
+   res = email.send()
+   return HttpResponse('%s'%res)
